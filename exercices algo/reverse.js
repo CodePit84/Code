06faf44c03e443b61517eae87b5452cb045769ff
CodePit84 <104715795+CodePit84@users.noreply.github.com>
2022-07-12 08:32:51 +0200

    phrase à l’envers.
    Ecrire un algorithme qui vous demande d'entrer une phrase et qui affiche cette phrase à l’envers.

---
+++ exercices algo/reverse.js
@@ -0,0 +1,10 @@
+const phrase = "il etait une fois une princesse";
+
+const mots = phrase.split (' ');
+
+//console.log(mots[3]);
+//console.log(mots[0]);
+
+let reverse = mots[5] + ' ' + mots[4] + ' ' + mots[3] + ' ' + mots[2] + ' ' + mots[1] + ' ' + mots[0];
+
+console.log(reverse);
