356aa80b36739e480d2c3922c83d182da39d553d
CodePit84 <104715795+CodePit84@users.noreply.github.com>
2022-07-18 13:11:36 +0200

    ajout description
    Écrire un algorithme permettant de faire la somme des N premiers entiers. La fonction demandée prend en entrée un nombre entier, N, et renvoie un autre nombre entier, la somme demandée.

--- exercices algo/SommeNentiers.js
+++ exercices algo/SommeNentiers.js
@@ -1,3 +1,5 @@
+//Écrire un algorithme permettant de faire la somme des N premiers entiers. La fonction demandée prend en entrée un nombre entier, N,
+// et renvoie un autre nombre entier, la somme demandée.
 let entierN = 100;
 
 function calculEntiers(){
